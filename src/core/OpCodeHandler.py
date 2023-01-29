e6f473eefd6a16c3d33b8de4d83f1c46211dbe72
Domenique Hunter <domeniquehunter@gmx.de>
2023-01-29 10:05:41 +0100

    rejoining channels (fixed?)

--- src/core/OpCodeHandler.py
+++ src/core/OpCodeHandler.py
@@ -8,21 +8,23 @@ from lib.Counter.Counter    import Counter as Counter
 import json, asyncio, time
 from lib.Channel.Channel import Channel
 
+
 class OpCodeHandler(ChatCodeHandler):
     """
         Control
     """
+
     def __init__(self, config, root_path):
         ChatCodeHandler.__init__(self, config, root_path)
-        
+
         # for the future
         self.core = None
         self.chat_code_handle = None
-        
+
         self.loop = asyncio.get_event_loop()
         self.counter_load_channels = Counter(10)
         self.counter_save_all = Counter (60)
-        
+
         # Register Opcode Actions
         self.opcodes_handler = Reactions()
         self.opcodes_handler.add_action("EXCEPTION"               , self._opcode_handler_except)
@@ -36,87 +38,80 @@ class OpCodeHandler(ChatCodeHandler):
         self.opcodes_handler.add_action(opcode.STATUS             , self._opcode_user_changed_status)
         self.opcodes_handler.add_action(opcode.JOIN_CHANNEL       , self._opcode_handler_user_joined_channel)
         self.opcodes_handler.add_action(opcode.LEAVE_CHANNEL      , self._opcode_hander_user_left_channel)
-        
-        self.opcodes_handler.add_action(opcode.INITIAL_CHANNEL_DATA, self._opcode_handler_inital_channel_data)        
+
+        self.opcodes_handler.add_action(opcode.INITIAL_CHANNEL_DATA, self._opcode_handler_inital_channel_data)
         self.opcodes_handler.add_action(opcode.LIST_PRIVATE_CHANNELS, self._opcode_handler_receive_list_of_open_public_channels)
         self.opcodes_handler.add_action(opcode.LIST_OFFICAL_CHANNELS, self._opcode_handler_receive_list_of_official_channels)
-        
+
         self.public_msg_handler = CommandManager(self.manpage)
         self.public_msg_handler.add_action("EXCEPTION", self._opcode_handler_except, no_help=True)
         self.public_msg_handler.add_action("!help", self._hook_help_page, no_help=True)
-        
+
         self.public_msg_handler.add_action("!debug_users", self._debug_users, "DEBUG show all users in channel", "owner", "Bot (Admin)")
-        
+
         self.all_users = {}
 
-                
-    async def dispatcher(self, op, json_object = None):
+    async def dispatcher(self, op, json_object=None):
         if op:
             await self.opcodes_handler.react(op, json_object)
-            
+
     async def _connect(self):
         await self.connect(self.config.server, self.config.port)
         await self.identify()
-        
-        
+
     async def _restart(self):
         print("MSG: restart chatbot")
         await self._connect()
         await self.channel_manager.rejoin_channels()
         self.restarts += 1
-        
+
     async def _prepare(self):
-        # settings
         await self._load_all_settings(self.owner)
-        
-        # requests
+
         await self._order_list_of_open_private_channels()
         await self._order_list_of_official_channels()
-        
-        # channel stuff
-        #self.channel_manager.reset_joined_channels()
+
         await self.join_default_channels(self.config.default_channels)
-        
+
     def start(self):
         self.loop.run_until_complete(self._connect())
         self.loop.run_until_complete(self._prepare())
         self.loop.run_until_complete(self._run())
-        
+
         self.loop.run_forever()
-                    
+
     async def _run (self):
-        
+
         while True:
             if self.connection != None and str(self.connection.state.name) == "OPEN":
                 message = await self._read()
                 if message:
-                    data = message.split(" ",1)
-                    
+                    data = message.split(" ", 1)
+
                     await self.dispatcher(*data)
-                    
-                    
+
                 if self.stop_impulse:
                     exit()
-                    
+
             else:
-                print ("!!!!!!!! RECONNECT !!!!!!!!")
+                print("!!!!!!!! RECONNECT !!!!!!!!")
                 await self._save_all_settings(self.owner)
-                time.sleep(30)                        
+                time.sleep(30)
                 await self._restart()
                 await self._load_all_settings(self.owner)
-                    
+
     async def _opcode_handler_private_message(self, json_object):
-        data = json.loads(json_object)        
-        user    = data['character']
+        data = json.loads(json_object)
+        user = data['character']
         message = data['message'].strip()
-        
-        message = message.split(' ',1)
+
+        message = message.split(' ', 1)
         handler = message.pop(0)
-        
+
         print (f"user: {user}\nmessage: {message}")
-        
+
         await self.private_msg_handler.react(handler, user, *message)
-    
+
     async def _opcode_handler_channel_message(self, json_object):
         '''
             Channel Message Dispatcher
@@ -124,71 +119,70 @@ class OpCodeHandler(ChatCodeHandler):
         data = json.loads(json_object)
         channel = data ['channel']
         message = data ['message'].strip()
-        user    = data ['character']
-        
-        message = message.split(' ',1)
+        user = data ['character']
+
+        message = message.split(' ', 1)
         handler = message.pop(0)
-        
+
         await self.public_msg_handler.react(handler, user, channel, *message)
-    
-    async def _opcode_handler_ping(self, nothing = None):
-        #print ("PING ... .. .. ...")
+
+    async def _opcode_handler_ping(self, nothing=None):
+        # print ("PING ... .. .. ...")
         await self._ping()
         await self.trigger_clock()
-        
+
         if self.counter_load_channels.tick():
             await self._order_list_of_open_private_channels()
             await self._order_list_of_official_channels()
-            
+
         if self.counter_save_all.tick():
             await self._hook_save_all(self.owner)
-            
-    
+
     async def _opcode_handler_channeldescription(self, json_object):
-        await self._get_channel_description(json.loads(json_object))    
-    
+        await self._get_channel_description(json.loads(json_object))
+
     async def _opcode_handler_except(self, json_object):
         # silent
         pass
-    
+
     async def _opcode_handler_invite(self, json_object):
         data = json.loads(json_object)
-        user    = data['sender']
-        title   = data['title']
+        user = data['sender']
+        title = data['title']
         channel = data['name']
-        
-        if self.is_priviliged(user):            
+
+        if self.is_priviliged(user):
             await self._hook_join_by_id(user, channel)
-            await self._hook_channel_name(user, channel+" "+title)
+            await self._hook_channel_name(user, channel + " " + title)
 
     async def _opcode_handler_user_joined_channel(self, json_object):
         data = json.loads(json_object)
         code = data['channel']
-        title= data['title']
-        user = data['character']['identity']    
-        
+        title = data['title']
+        user = data['character']['identity']
+
         if code not in self.channels:
             channel = Channel(title, code)
             self.channels[code] = channel
-            
+
             if self.channel_creation_queue.size() > 0:
                 for index, channel_object in enumerate(self.channel_creation_queue.queue):
                     if channel_object["name"] == channel.name:
                         channel_object = self.channel_creation_queue.pop(index)
-                        
+
                         await self._invite_user_to_channel_by_code(channel_object["user"], code)
                         await self.channel_operator(channel_object["user"], code)
-                        
+
                         self.channels[code].add_admin(channel_object["user"])
                         self.channels[code].persistent = False
-                    
+
         self.channels[code].add_character(user.lower())
 
     async def _opcode_hander_user_left_channel(self, json_object):
         data = json.loads(json_object)
         code = data['channel']
         user = data['character']
-        
+
         try:
             leave = self.channels[code].remove_character(user.lower())
             if leave:
@@ -197,39 +191,37 @@ class OpCodeHandler(ChatCodeHandler):
             print (f"channel {code} not found")
 
     async def _opcode_handler_kicked(self, json_object):
-        data      = json.loads(json_object)
-        operator  = data['operator']
-        channel   = data['channel']
-        character = data['character']       
-        
-        #print ("Operator %s kicked %s from %s" % (operator, character, channel))
+        data = json.loads(json_object)
+        operator = data['operator']
+        channel = data['channel']
+        character = data['character']
+
+        # print ("Operator %s kicked %s from %s" % (operator, character, channel))
         if (character.lower() == self.charactername.lower()):
             self.channels.pop(channel)
-            
+
     async def _opcoce_user_disconnected(self, json_object):
         data = json.loads(json_object)
         for ch_key in self.channels:
             self.channels[ch_key].remove_character(data['character'].lower())
-    
+
     async def _opcode_user_changed_status(self, json_object):
         pass
-    
+
     async def _opcode_user_connected(self, json_object):
         data = json.loads(json_object)
-        self.all_users[data['identity'].lower()] =  data
-    
+        self.all_users[data['identity'].lower()] = data
+
     async def _opcode_handler_inital_channel_data(self, json_object):
         data = json.loads(json_object)
-        #print (data)
+        # print (data)
         channel = data['channel']
         for user in data['users']:
             self.channels[channel].add_character(user['identity'].lower())
-    
+
     async def _opcode_handler_receive_list_of_open_public_channels(self, json_object):
         self.channel_manager.add_open_private_channels(json_object)
-        
-    async def _opcode_handler_receive_list_of_official_channels(self, json_object):        
+
+    async def _opcode_handler_receive_list_of_official_channels(self, json_object):
         self.channel_manager.add_official_channels(json_object)
-    
-    
-        
+
--- src/lib/Channel/Channels.py
+++ src/lib/Channel/Channels.py
@@ -5,98 +5,97 @@ import time
 from lib.Channel.Channel import Channel
 from lib.Counter.Counter import Counter
 
+
 class ChannelManager(object):
     """
         Dataclass to store channels
         TODO: rename into ChannelManager
     
-    """    
-    
-    def __init__(self, join_method = None):
+    """
+
+    def __init__(self, join_method=None):
         self.open_private_channels = {}
         self.official_channels = {}
-        
+
         self.joined_channels = {}
-        
+
         self.join_method = join_method
         self.counter = Counter(2)
-        
+
     def add_open_private_channels(self, json_object):
         start = time.time()
         data = json.loads(json_object)
-        
+
         for channel_data in data["channels"]:
             code = channel_data["name"]
             name = channel_data["title"]
 
             if not code in self.open_private_channels:
                 self.open_private_channels[code] = Channel(name, code)
-                
+
         elapsed = time.time() - start
         print(f"open private channels ({len(self.open_private_channels)}) in {elapsed}s")
-             
+
     def add_official_channels(self, json_object):
         start = time.time()
         data = json.loads(json_object)
-        
+
         for channel_data in data["channels"]:
             name = channel_data["name"]
-            
+
             if not name in self.official_channels:
-                self.official_channels[name] = Channel(name, name) 
+                self.official_channels[name] = Channel(name, name)
 
         elapsed = time.time() - start
         print(f"official channels ({len(self.official_channels)}) in {elapsed}s")
-    
+
     def add_channel(self, channel:Channel) -> Channel:
         self.joined_channels[channel.code] = channel
         return self.joined_channels[channel.code]
-    
+
     def reset_joined_channels(self):
         self.joined_channels = {}
-    
+
     def reset_open_private(self):
         del self.open_private_channels
         self.open_private_channels = {}
-        
+
     def find_channel(self, name:str) -> Channel:
         for key, channel in self.open_private_channels.items():
             if channel.name.lower() == name.lower():
                 return channel
-            
-        
+
         if name in self.official_channels:
             return self.official_channels[name]
-        
+
         else:
             return None
-        
+
     def find_channel_by_id(self, code:str) -> Channel:
         if code in self.open_private_channels:
             return self.open_private_channels[code]
-        
+
         elif code in self.official_channels:
             return  self.official_channels[code]
-        
+
         else:
             return None
-    
+
     async def join(self, name:str, code:str=None):
         if name and code:
             channel = Channel(name, code)
             await self.join_method(channel.code, channel.name)
             self.add_channel(channel)
             return channel.name
-        
+
         elif name and not code:
             return await self.join_by_name(name)
-        
+
         elif not name and code:
             return await self.join_by_id(code)
-                
-            
+
         return None
-    
+
     async def join_by_name(self, name:str):
         channel = self.find_channel(name)
         if self.join_method:
@@ -104,9 +103,9 @@ class ChannelManager(object):
                 await self.join_method(channel.code, channel.name)
                 self.add_channel(channel)
                 return channel.name
-            
+
         return None
-        
+
     async def join_by_id(self, code:str):
         channel = self.find_channel_by_id(code)
         if self.join_method:
@@ -115,55 +114,29 @@ class ChannelManager(object):
             return channel.name
         else:
             return None
-        
-    
+
     # TODO: test rejoin
     async def rejoin(self, channel:Channel):
         if isinstance(channel, Channel) and channel.code in self.joined_channels:
-            await self.join_method(channel.code, channel.name)
+            await self.join_method(channel.code, channel.name, force=True)
             return channel.code
-        
+
         elif isinstance(channel, str):
             await self.join_method(channel, channel)
             print(f"WARNING rejoin: {channel} is not of type Channel but of string")
             return channel.code
-                
+
         else:
             return None
-    
+
     async def rejoin_channels(self):
         for channel in self.joined_channels.values():
             await self.rejoin(channel)
-        
+
     def clock(self):
         if self.counter.tick():
             for channel in self.joined_channels.values():
                 # trigger a clock method if channel is not persistant
                 if not channel.persistent:
                     pass
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
