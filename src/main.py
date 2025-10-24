0ae9c5e67bbc7c05ea5c7140f310199efacaeff6
Domenique Hutner <domeniquehunter@gmx.de>
2025-10-24 12:42:48 +0200

    using pathlib in main and little cleanup

--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
-import os
-
 from framework import Client
 from framework.lib.config import Config
 
-# from webserver.Webserver import Webserver
+from pathlib import Path
 
-PROJECT_ROOT = os.path.dirname(__file__)
-config = Config(PROJECT_ROOT + '/config.json')
+PROJECT_ROOT = Path(__file__).parent
 
-client = Client(config, PROJECT_ROOT + '/data')
+config_path = PROJECT_ROOT / 'config.json'
+config = Config(str(config_path))
 
-client.enable_plugin_loader(plugins_dir=f"{PROJECT_ROOT}/plugins/")
+data_path = PROJECT_ROOT / 'data'
+client = Client(config, str(data_path))
+
+plugins_path = PROJECT_ROOT / 'plugins'
+client.enable_plugin_loader(plugins_dir=plugins_path)
 client.load_plugins()
 
 # start webserver in thread
+# from webserver.Webserver import Webserver
 # Webserver(client)
 
 # forever!
